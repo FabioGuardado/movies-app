77ddfeb69b8b956e7702c891c0ebace19b4ad752
Fabio Guardado <fabio.guardado2012@gmail.com>
2021-11-25 11:54:10 -0600

    :hammer: Create project pages' components

---
+++ src/pages/MovieDetails/MovieDetails.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MovieDetails: React.FunctionComponent = () => {
+  return (
+    <>
+      <h1>MovieDetails</h1>
+    </>
+  );
+};
+
+export default MovieDetails;
---
+++ src/pages/SeasonDetails/SeasonDetails.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SeasonDetails: React.FunctionComponent = () => {
+  return (
+    <>
+      <h1>Season Details</h1>
+    </>
+  );
+};
+
+export default SeasonDetails;
