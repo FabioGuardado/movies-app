43364fd1b70b4c9fa1c8aa4df0597726192c1e4a
Fabio Guardado <fabio.guardado2012@gmail.com>
2021-12-01 16:20:12 -0600

    :hammer: Add function to format large int quantities

---
+++ src/utils/formatQuantities.ts
@@ -0,0 +1,26 @@
+const formatQuantities = (numberToFormat: number) => {
+  if (numberToFormat > 999) {
+    var newNumber = '';
+    let transformedNumber = numberToFormat.toString();
+    let counter: number = 0;
+    for (let i = transformedNumber.length - 1; i >= 0; i--) {
+      if (counter === 3) {
+        newNumber = newNumber + ',';
+        counter = 1;
+      } else {
+        counter = counter + 1;
+      }
+      let currentNumber = transformedNumber.charAt(i);
+      newNumber = newNumber + currentNumber.toString();
+    }
+
+    let newNumberFormated = '';
+    for (let i = newNumber.length; i >= 0; i--) {
+      newNumberFormated = newNumberFormated + newNumber.charAt(i - 1);
+    }
+    return newNumberFormated;
+  }
+  return numberToFormat.toString();
+};
+
+export default formatQuantities;
