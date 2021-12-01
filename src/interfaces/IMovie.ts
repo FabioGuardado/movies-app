81a59cf7ebf58cbf56e5c8549c4b00b14a0fc600
Fabio Guardado <fabio.guardado2012@gmail.com>
2021-12-01 16:15:00 -0600

    :hammer: Add movie-related interfaces

---
+++ src/interfaces/IMovie.ts
@@ -0,0 +1,48 @@
+import IGenre from './IGenre';
+
+interface IMovie {
+  adult: boolean;
+  backdrop_path: string | null;
+  belongs_to_collection: null | {};
+  budget: number;
+  genres: IGenre[];
+  homepage: string | null;
+  id: number;
+  imdb_id: string | null;
+  original_language: string;
+  original_title: string;
+  overview: string | null;
+  popularity: number;
+  poster_path: string | null;
+  production_companies: ProductionCompany[];
+  production_countries: ProductionCountry[];
+  release_date: Date | string;
+  revenue: number;
+  runtime: number | null;
+  spoken_languages: SpokenLanguage[];
+  status: string;
+  tagline: string | null;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export default IMovie;
+
+interface ProductionCompany {
+  id: number;
+  logo_path: null | string;
+  name: string;
+  origin_country: string;
+}
+
+interface ProductionCountry {
+  iso_3166_1: string;
+  name: string;
+}
+
+interface SpokenLanguage {
+  iso_639_1: string;
+  name: string;
+}
