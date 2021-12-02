f9ab25de31d1d58f76831c9353243d77bd888a15
Fabio Guardado <fabio.guardado2012@gmail.com>
2021-12-01 21:05:22 -0600

    :hammer: Add type guards to get shows details

--- src/utils/TypeGuards.ts
+++ src/utils/TypeGuards.ts
@@ -1,5 +1,7 @@
 import ICast from '../interfaces/ICast';
 import ICrew from '../interfaces/ICrew';
+import IMovie from '../interfaces/IMovie';
+import IShow from '../interfaces/IShow';
 import { IMovieSummary, IShowSummary } from '../interfaces/ISummaries';
 
 export const getNameOrTitle = (element: IMovieSummary | IShowSummary) => {
@@ -22,5 +24,40 @@ export const getDetailsLink = (element: IMovieSummary | IShowSummary) => {
 
 export const isCast = (element: ICast | ICrew) => {
   if ('character' in element) return element.character;
+  return null;
+};
+
+export const isMovie = (element: IMovie | IShow) => {
+  if ('title' in element) return true;
+  return false;
+};
+
+export const isShow = (element: IMovie | IShow) => {
+  if ('name' in element) return true;
+  return false;
+};
+
+export const getOriginalNameOrTitle = (element: IShow | IMovie) => {
+  if ('original_name' in element) return element.original_name;
+  return element.original_title;
+};
+
+export const getBudget = (element: IShow | IMovie) => {
+  if ('budget' in element) return element.budget;
+  return null;
+};
+
+export const getRevenue = (element: IShow | IMovie) => {
+  if ('revenue' in element) return element.revenue;
+  return null;
+};
+
+export const getType = (element: IShow | IMovie) => {
+  if ('type' in element) return element.type;
+  return null;
+};
+
+export const getNetworkPath = (element: IShow | IMovie) => {
+  if ('networks' in element) return element.networks[0].logo_path;
   return null;
 };
