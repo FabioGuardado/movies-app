810bc18de0232cff4c9a1b22703bbf8190f7e5b4
Fabio Guardado <fabio.guardado2012@gmail.com>
2021-12-01 21:04:19 -0600

    :hammer: Add details-related fetch methods

--- src/API/shows.ts
+++ src/API/shows.ts
@@ -1,5 +1,8 @@
 import { get } from '../helpers/requestService';
+import ICredits from '../interfaces/ICredits';
 import IResponse from '../interfaces/IResponse';
+import IReview from '../interfaces/IReview';
+import IShow from '../interfaces/IShow';
 import { IShowSummary } from '../interfaces/ISummaries';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
@@ -7,6 +10,33 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 export async function getPopularShows() {
   const response = await get<IResponse<IShowSummary>>(
     `/tv/popular?api_key=${API_KEY}`,
+  );
+
+  return response;
+}
+
+export async function getShowById(showId: number) {
+  const response = await get<IShow>(`/tv/${showId}?api_key=${API_KEY}`);
+  return response;
+}
+
+export async function getCreditsByShowId(showId: number) {
+  const response = await get<ICredits>(
+    `/tv/${showId}/credits?api_key=${API_KEY}`,
+  );
+  return response;
+}
+
+export async function getReviewsByShowId(showId: number) {
+  const response = await get<IResponse<IReview>>(
+    `/tv/${showId}/reviews?api_key=${API_KEY}`,
+  );
+  return response;
+}
+
+export async function getSimilarShows(showId: number) {
+  const response = await get<IResponse<IShowSummary>>(
+    `/tv/${showId}/similar?api_key=${API_KEY}`,
   );
 
   return response;
