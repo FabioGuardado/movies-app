3056ef4192f334026984c4e2d9f54e9309df4bcf
Fabio Guardado <fabio.guardado2012@gmail.com>
2021-12-03 15:49:06 -0600

    :hammer: Add ISeasonSummary interface

--- src/interfaces/ISummaries.ts
+++ src/interfaces/ISummaries.ts
@@ -30,3 +30,13 @@ export interface IShowSummary {
   name: string;
   original_name: string;
 }
+
+export interface ISeasonSummary {
+  air_date: string;
+  episode_count: number;
+  id: number;
+  name: string;
+  overview: string;
+  poster_path: string;
+  season_number: number;
+}
