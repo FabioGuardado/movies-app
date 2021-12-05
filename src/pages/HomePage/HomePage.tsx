54303b26ebbe83eeccfad1a80af7923137f5fe3e
Fabio Guardado <fabio.guardado2012@gmail.com>
2021-12-04 22:47:01 -0600

    :sparkles: Add search section to home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { getPopularMovies } from '../../API/movies';
 import { getPopularShows } from '../../API/shows';
 import HomePageCard from '../../components/HomePage/Card/Card';
+import SearchSection from '../../components/HomePage/SearchSection/SearchSection';
 import Layout from '../../components/UI/Layout/Layout';
 import Loader from '../../components/UI/Loader/Loader';
 import { IMovieSummary, IShowSummary } from '../../interfaces/ISummaries';
@@ -30,13 +31,15 @@ const HomePage: React.FunctionComponent = () => {
 
   return movies && shows ? (
     <Layout>
+      <SearchSection />
+
       <section className="mb-5">
-        <div className="p-3 bg-gray-200 rounded-sm my-5">
+        <div className="p-3 bg-gray-200 rounded-md my-5">
           <p className="text-xl ml-2">
             🔥 <span className="font-bold">Popular movies</span>
           </p>
         </div>
-        <div className="grid grid-cols-2 gap-4 lg:gap-6 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        <div className="horizontal-item-list mb-3 h-auto pl-2 pb-5 flex flex-row flex-nowrap overflow-x-scroll">
           {movies &&
             movies.map((movie: IMovieSummary) => (
               <HomePageCard key={movie.id} data={movie} />
@@ -45,12 +48,12 @@ const HomePage: React.FunctionComponent = () => {
       </section>
 
       <section className="mb-5">
-        <div className="p-3 bg-gray-200 rounded-sm my-5">
+        <div className="p-3 bg-gray-200 rounded-md my-5">
           <p className="text-xl ml-2">
             🔥 <span className="font-bold">Popular TV shows</span>
           </p>
         </div>
-        <div className="grid grid-cols-2 gap-4 lg:gap-6 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        <div className="horizontal-item-list mb-3 h-auto pl-2 pb-5 flex flex-row flex-nowrap overflow-x-scroll">
           {shows &&
             shows.map((show: IShowSummary) => (
               <HomePageCard key={show.id} data={show} />
