b9102c29ce3d622a2116b3ed3b908d51b563f6dd
Fabio Guardado <fabio.guardado2012@gmail.com>
2021-12-06 11:54:22 -0600

    :sparkles: Update GenreTag link to redirect to movies page with the filter

--- src/components/UI/GenreTag/GenreTag.tsx
+++ src/components/UI/GenreTag/GenreTag.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import routes from '../../../routes/routes';
 import GenreTagProps from '../../../types/GenreTagProps';
 
-const GenreTag: React.FunctionComponent<GenreTagProps> = ({ genre }) => {
+const GenreTag: React.FunctionComponent<GenreTagProps> = ({
+  genre,
+  element_type,
+}) => {
   return (
-    <Link to={`/search?tag=${genre.id}`}>
+    <Link
+      to={`${element_type === 'movie' ? routes.MOVIE : routes.SHOW}?genre=${
+        genre.id
+      }`}
+    >
       <div className="mr-1 mt-2 sm:mt-0 py-1 px-4 text-sm text-white border border-solid border-white rounded-full transition-all hover:bg-white hover:text-gray-600">
         {genre.name}
       </div>
