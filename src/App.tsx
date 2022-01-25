1a81e2dd1e9494c68320e3bd923e6ed240f02a44
Fabio Guardado <fabio.guardado2012@gmail.com>
2022-01-25 11:27:31 -0600

    :hammer: Add ModalContext to the app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { ProvideAuth } from './context/authContext';
 import { AlertProvider } from './context/alertContext';
+
 import AppRouter from './routes/AppRouter';
 import Alert from './components/UI/Alert/Alert';
+import { ModalProvider } from './context/modalContext';
+import MainModal from './components/UI/Modals/MainModal/MainModal';
+
 function App() {
   return (
     <ProvideAuth>
-      <AlertProvider>
-        <AppRouter />
-        <Alert />
-      </AlertProvider>
+      <ModalProvider>
+        <AlertProvider>
+          <AppRouter />
+          <Alert />
+          <MainModal />
+        </AlertProvider>
+      </ModalProvider>
     </ProvideAuth>
   );
 }
