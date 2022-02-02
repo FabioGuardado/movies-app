f7267f1101679cf6abfcb82c7740057c49ff3be6
Fabio Guardado <fabio.guardado2012@gmail.com>
2022-02-02 09:58:34 -0600

    :recycle: Refactor alert component

--- src/components/UI/Alert/Alert.tsx
+++ src/components/UI/Alert/Alert.tsx
@@ -1,20 +1,24 @@
-import { useContext } from 'react';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { createPortal } from 'react-dom';
-import AlertContext from '../../../context/alertContext';
+import useAlertContext from '../../../hooks/useAlertContext';
 
 const domPortalNode = document.getElementById('alert');
 
 const Alert = () => {
-  const alertContext = useContext(AlertContext);
+  const { alert, clearAlerts } = useAlertContext();
 
-  if (alertContext?.alert !== 'NONE' && domPortalNode) {
+  if (alert.status !== 'NONE' && domPortalNode) {
     return createPortal(
       <div
-        className={`text-white p-5 font-bold text-lg absolute bottom-5 left-8 z-50 rounded shadow-2xl ${
-          alertContext?.alert === 'SUCCESS' && 'bg-green-700'
-        } ${alertContext?.alert === 'ERROR' && 'bg-red-700'}`}
+        className={`fixed text-white p-5 font-bold text-lg bottom-5 left-8 z-50 rounded shadow-2xl ${
+          alert.status === 'SUCCESS' && 'bg-green-600 hover:bg-green-700'
+        } ${alert.status === 'ERROR' && 'bg-red-600 hover:bg-red-700'}`}
       >
-        {alertContext?.alertText}
+        <span className="mr-4">{alert.body}</span>
+        <button onClick={() => clearAlerts()}>
+          <FontAwesomeIcon icon={faTimes} />
+        </button>
       </div>,
       domPortalNode,
     );
