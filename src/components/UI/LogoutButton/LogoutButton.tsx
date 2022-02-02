d3074b333b8fc5f2af446cc263f442defdac61fa
Fabio Guardado <fabio.guardado2012@gmail.com>
2022-02-02 11:23:54 -0600

    :recycle: Refactor modal context implementation

--- src/components/UI/LogoutButton/LogoutButton.tsx
+++ src/components/UI/LogoutButton/LogoutButton.tsx
@@ -1,10 +1,10 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import useAuth from '../../../hooks/useAuth';
-import ModalContext from '../../../context/modalContext';
+import useModalContext from '../../../hooks/useModalContext';
 
 const LogoutButton: React.FunctionComponent = () => {
   const { logout } = useAuth();
-  const modalContext = useContext(ModalContext);
+  const { setModalBody } = useModalContext();
 
   const logoutModalBody = (
     <div className="w-full flex flex-col items-center justify-center">
@@ -20,7 +20,7 @@ const LogoutButton: React.FunctionComponent = () => {
         </button>
         <button
           className="py-2 px-4 bg-red-600 text-white text-lg rounded-lg shadow-md"
-          onClick={() => modalContext?.setModalBody(null)}
+          onClick={() => setModalBody(null)}
         >
           Cancel
         </button>
@@ -31,7 +31,7 @@ const LogoutButton: React.FunctionComponent = () => {
   return (
     <button
       className="px-4 py-2 rounded-md bg-red-600 transition-all hover:bg-red-500"
-      onClick={() => modalContext?.setModalBody(logoutModalBody)}
+      onClick={() => setModalBody(logoutModalBody)}
     >
       Logout
     </button>
